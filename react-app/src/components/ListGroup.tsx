443041cc5a4eab890cd5869143f5604d3c77e5c9
FraNzY <anoj1810@gmail.com>
2023-06-03 00:08:51 +0545

    react 18 with project

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,23 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
+  let items = ["Kathmandu", "Pokhara", "Mustang", "Manang", "Dharan"];
+
+  //event handling
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Name</h1>
+      {items.length === 0 && <p>No List Items</p>}
+      <ul className="list-group">
+        {items.map((items, index) => (
+          <li className="list-group-item" key={items} onClick={handleClick}>
+            {items}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
